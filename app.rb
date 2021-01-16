acfc01a5be5104c374d7311ca61792ea60189def
lrh2daizzo <herrera.lrh@gmail.com>
2021-01-15 21:26:38 -0600

    Done.

--- app.rb
+++ app.rb
@@ -15,12 +15,13 @@ class App < Sinatra::Base
   end
 
   get '/first_exercise' do
-    "Your first exercise will be to set your session key-value pair.\nIn the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.\nThen, navigate to the '/set' path."
+    "Your first exercise will be to set your session key-value pair.\nIn the route: get '/set', write a line of code that sets the :foo key of the session hash equal to 'hello'.\nThen, naviate to the '/set' path."
   end
 
   get '/set' do
     # set the :foo key of the session hash equal to 'hello' here!
     session[:foo] = 'hello'
+
     if session[:foo] == 'hello'
       redirect '/fetch'
     else
@@ -29,18 +30,18 @@ class App < Sinatra::Base
   end
 
   get '/fetch' do
-    "You did it! session[:foo] value: #{session[:foo]}.\nMove on to Part II of this lab at '/second_exercise' "
+    "You did it! session[:foo] value: #{session[:foo]}.\nMove on to Part II of this lab."
   end
 
   get '/second_exercise' do
-    "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_session, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
+    "Your second lesson will be to set your session :id key to a value.\nIn the route: /set_ession, write a line of code that sets the session[:id] equal to 1.\nThen, navigate to the '/set_session' path"
   end
 
   get '/set_session' do
+    # Set session[:id] here!
     session[:id] = 1
 
     if session[:id] == 1
-      # "Session ID set. It's currently set to #{session[:id]}."
       redirect '/fetch_session_id'
     else
       "Session ID has not been set!"
@@ -52,7 +53,8 @@ class App < Sinatra::Base
   end
 
   get '/logout' do
-    session.clear
+    # clear the session hash here.
+    session.destroy 
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
